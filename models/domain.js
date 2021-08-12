cb7234584a0a7632767db996be5266b6f629c793
sammyhy <sammykkoon@gmail.com>
2021-08-12 17:54:11 +0900

    first commit

---
+++ models/domain.js
@@ -0,0 +1,34 @@
+const { Sequelize } = require("sequelize");
+const sequelize = require("sequelize");
+
+module.exports = class Domain extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        host: {
+          type: Sequelize.toString(80),
+          allowNull: false,
+        },
+        type: {
+          type: Sequelize.ENUM("free", "premium"),
+          allowNull: false,
+        },
+        clientSecret: {
+          type: Sequelize.UUID,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        paranoid: true,
+        modelName: "Domain",
+        tableName: "domains",
+      }
+    );
+  }
+
+  static associate(db) {
+    db.Domain.belongsTo(db.User);
+  }
+};
