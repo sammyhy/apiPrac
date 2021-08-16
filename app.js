6a21c9561e74f625c392b742feab7d393e8c84cc
sammy <sammykkoon@gmail.com>
2021-08-16 21:53:07 +0900

    learning~~

--- app.js
+++ app.js
@@ -9,26 +9,26 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 const authRouter = require("./routes/auth");
-const indexRouter = require("./routes/index");
+const indexRouter = require("./routes");
 const { sequelize } = require("./models");
 const passportConfig = require("./passport");
 
 const app = express();
 passportConfig();
-app.set("port", process.env.env_PORT || 3001);
+app.set("port", process.env.env_PORT || 8002);
 app.set("view engine", "html");
 nunjucks.configure("views", {
-  express: app,
-  watch: true,
+    express: app,
+    watch: true,
 });
 sequelize
-  .sync({ force: false })
-  .then(() => {
-    console.log("database connect success!");
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+    .sync({ force: false })
+    .then(() => {
+        console.log("database connect success!");
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
@@ -36,15 +36,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-      httpOnly: true,
-      secure: false,
-    },
-  })
+    session({
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.COOKIE_SECRET,
+        cookie: {
+            httpOnly: true,
+            secure: false,
+        },
+    }),
 );
 
 app.use(passport.initialize());
@@ -53,18 +53,18 @@ app.use("./auth", authRouter);
 app.use("/", indexRouter);
 
 app.use((req, res, next) => {
-  const error = new Error(`${req.method} ${req.url} router does not exist`);
-  error.status = 404;
-  next(error);
+    const error = new Error(`${req.method} ${req.url} router does not exist`);
+    error.status = 404;
+    next(error);
 });
 
 app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
-  res.status(err.status || 500);
-  res.render("error");
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 app.listen(app.get("port"), () => {
-  console.log(app.get("port"), "번 포트 대기중");
+    console.log(app.get("port"), "번 포트 대기중");
 });
